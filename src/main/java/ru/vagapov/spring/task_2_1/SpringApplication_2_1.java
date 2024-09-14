dd8adf35b32235a68e6c55a6d465d9cbf358a228
Dayan <disyangulov@yandex.ru>
2024-09-14 19:51:04 +0500

    task 2.1 done

--- src/main/java/ru/vagapov/spring/task_2_1/SpringApplication_2_1.java
+++ src/main/java/ru/vagapov/spring/task_2_1/SpringApplication_2_1.java
@@ -3,6 +3,7 @@ package ru.vagapov.spring.task_2_1;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.vagapov.spring.task_2_1.config.AppConfig;
+import ru.vagapov.spring.task_2_1.model.Cat;
 import ru.vagapov.spring.task_2_1.model.HelloSpring;
 
 public class SpringApplication_2_1 {
@@ -12,7 +13,12 @@ public class SpringApplication_2_1 {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloSpring bean =
                 (HelloSpring) applicationContext.getBean("helloSpring");
-        System.out.println(bean.getMessage());
+        HelloSpring anotherBean =
+                (HelloSpring) applicationContext.getBean("helloSpring");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat anotherCatBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == anotherBean);
+        System.out.println(catBean == anotherCatBean);
     }
 
 }
---
+++ src/main/java/ru/vagapov/spring/task_2_1/model/Cat.java
@@ -0,0 +1,5 @@
+package ru.vagapov.spring.task_2_1.model;
+
+public class Cat {
+
+}
