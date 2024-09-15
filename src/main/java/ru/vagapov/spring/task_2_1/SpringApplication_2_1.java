49b46b4c05260a9e88e76eed98baa7900caa6bf6
Dayan <disyangulov@yandex.ru>
2024-09-15 13:00:44 +0500

    Constants for Beans name Scope realized through ConfigurableBeanFactory

--- src/main/java/ru/vagapov/spring/task_2_1/SpringApplication_2_1.java
+++ src/main/java/ru/vagapov/spring/task_2_1/SpringApplication_2_1.java
@@ -6,17 +6,20 @@ import ru.vagapov.spring.task_2_1.config.AppConfig;
 import ru.vagapov.spring.task_2_1.model.Cat;
 import ru.vagapov.spring.task_2_1.model.HelloSpring;
 
+import static ru.vagapov.spring.task_2_1.config.AppConfig.CAT;
+import static ru.vagapov.spring.task_2_1.config.AppConfig.HELLO_WORLD;
+
 public class SpringApplication_2_1 {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloSpring bean =
-                (HelloSpring) applicationContext.getBean("helloSpring");
+                (HelloSpring) applicationContext.getBean(HELLO_WORLD);
         HelloSpring anotherBean =
-                (HelloSpring) applicationContext.getBean("helloSpring");
-        Cat catBean = (Cat) applicationContext.getBean("cat");
-        Cat anotherCatBean = (Cat) applicationContext.getBean("cat");
+                (HelloSpring) applicationContext.getBean(HELLO_WORLD);
+        Cat catBean = (Cat) applicationContext.getBean(CAT);
+        Cat anotherCatBean = (Cat) applicationContext.getBean(CAT);
         System.out.println(bean == anotherBean);
         System.out.println(catBean == anotherCatBean);
     }
--- src/main/java/ru/vagapov/spring/task_2_1/config/AppConfig.java
+++ src/main/java/ru/vagapov/spring/task_2_1/config/AppConfig.java
@@ -1,5 +1,6 @@
 package ru.vagapov.spring.task_2_1.config;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -8,16 +9,17 @@ import ru.vagapov.spring.task_2_1.model.HelloSpring;
 
 @Configuration
 public class AppConfig {
-
-    @Bean(name = "helloSpring")
+    public static final String HELLO_WORLD = "Hello World";
+    public static final String CAT = "Cat";
+    @Bean(name = HELLO_WORLD)
     public HelloSpring getHelloWorld() {
         HelloSpring helloWorld = new HelloSpring();
         helloWorld.setMessage("Hello Spring!");
         return helloWorld;
     }
 
-    @Bean(name = "cat")
-    @Scope("prototype")
+    @Bean(name = CAT)
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public Cat getCat() {
         return new Cat();
     }
